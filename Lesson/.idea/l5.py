3bbc1fe98ad85039fb8be231597615818ebb9b30
DanilBuliy <bulijdanil@gmail.com>
2023-07-02 11:12:46 +0300

    changed

--- Lesson/.idea/l5.py
+++ Lesson/.idea/l5.py
@@ -1,13 +1,20 @@
-name=input("Введите имя-> ")
-age=input("Введите свой возраст-> ")
-
-if not age.isdigit() or int(age)<=0:
-        print("Ошибка, повторите ввод")
-elif 0<int(age)<10:
-    print(f"Привет, шкет #{name}#")
-elif 10<=int(age)<=18:
-    print("Как жизнь #%s#?"%(name))
-elif 18 < int(age) < 100:
-    print("Что желаете #{}#?".format(name))
-else:
-    print(f"#{name}#, вы лжете - в наше время столько не живут...")
+while(True):
+    name=input("Введите имя-> ")
+    age=input("Введите свой возраст-> ")
+    if not age.isdigit() or int(age)<=0:
+            print("Ошибка, повторите ввод")
+    elif 0<int(age)<10:
+        print(f"Привет, шкет #{name}#")
+    elif 10<=int(age)<=18:
+        print("Как жизнь #%s#?"%(name))
+    elif 18 < int(age) < 100:
+        print("Что желаете #{}#?".format(name))
+    else:
+        print(f"#{name}#, вы лжете - в наше время столько не живут...")
+    res=input("Желаете выйти? (Д/Y)")
+    if res=="Д" or res=="Д".lower():
+        break
+    elif res=="Y" or res=="Y".lower():
+        continue
+    else:
+        print("непонятный текст")
