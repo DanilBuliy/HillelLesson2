a3436daa2bc633dac3560e1f72d96d3690bab641
DanilBuliy <bulijdanil@gmail.com>
2023-07-16 10:35:26 +0300

    changed

--- Lesson/.idea/l12.py
+++ Lesson/.idea/l12.py
@@ -1,4 +1,4 @@
 inputdata = ['Страна', 'шалаш', 'Летел', 'вертолёт', 'УЧУ', 'мэм', 'язык']
 
-inputdata2=tuple(filter(lambda x:x if isinstance(x,str) and x[::-1].lower()==x.lower() else "",inputdata))
+inputdata2=list(filter(lambda x:x if x[::-1].lower()==x.lower() else "",inputdata))
 print(inputdata2)
