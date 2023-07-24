7ef2787d7f152931324c2b85ccec0734faf2484b
DanilBuliy <bulijdanil@gmail.com>
2023-07-24 11:21:54 +0300

    changed

---
+++ Lesson/.idea/l17.py
@@ -0,0 +1,14 @@
+dict1={
+    123456:("John",30),
+    123457:("Dan",20),
+    123458:("Tom",44),
+    123459:("Dima",33),
+    123450:("Oleg",23),
+    123451:("Sam",27)
+}
+print(dict1)
+
+import json
+
+f=open("dict1.json","w")
+json.dump(dict1,f,indent=1)
