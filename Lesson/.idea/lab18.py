d131157b3ab9575f994384fade7ab71ab972b540
DanilBuliy <bulijdanil@gmail.com>
2023-07-24 19:59:42 +0300

    changed

---
+++ Lesson/.idea/lab18.py
@@ -0,0 +1,17 @@
+import json
+import csv
+import random
+
+
+with open("dict1.json", "r") as f:
+    data = json.load(f)
+
+with open("dict1.csv", "w", encoding="utf-8") as f1:
+    wr = csv.writer(f1)
+    wr.writerow(["ID", "Имя", "Возраст", "Телефон"])
+
+    pre = ["095", "066", "098", "096", "050", "097"]
+    numb = [random.randint(900000, 10000000) for i in range(len(data))]
+
+    for i, y in data.items():
+        wr.writerow([i, *y, f"{pre[random.randint(0, len(pre) - 1)]}{numb.pop()}"])
