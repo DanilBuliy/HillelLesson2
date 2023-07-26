c078d18eec45b6022260fdb75e005fd7e428bc91
DanilBuliy <bulijdanil@gmail.com>
2023-07-26 20:51:25 +0300

    changed

--- Lesson/.idea/l20.py
+++ Lesson/.idea/l20.py
@@ -26,8 +26,9 @@ o.birthday()
 
 class Truck(Auto):
 
-    def __init__(self,max_load):
-        self.maxl=max_load
+    def __init__(self,max_load,brand,mark,age,color="black",weight="2.5t"):
+        super().__init__(brand,mark,age,color="black",weight="2.5t")
+        self.maxl = max_load
 
     def move(self):
         print("attention")
@@ -41,25 +42,29 @@ class Truck(Auto):
 
 class Car(Auto):
 
-    def __init__(self, max_speed):
+    def __init__(self, max_speed,brand,mark,age,color="black",weight="2.5t"):
+        super().__init__(brand, mark, age, color="black", weight="2.5t")
         self.maxs = max_speed
 
     def move(self):
         super().move()
         print(f"max speed is {self.maxs}")
+
 ###Класс Трак####
-obj=Truck(2000)
+obj=Truck(2000,"Classic","Nissan",10)
 obj.move()
 obj.load()
 
-obj1=Truck(40000)
+obj1=Truck(40000,"Sport","Lanos",30)
 print("obj1 is",end=" ")
 obj1.stop()
+
 print("obj1 is",end=" ")
 obj1.load()
+
 ###Класс Car#####
-obj2=Car(400)
+obj2=Car(400,"Classic","Audi",10)
 obj2.move()
 
-obj3=Car(340)
+obj3=Car(340,"Sport++","Shevrolet Comaro",30)
 obj3.move()
