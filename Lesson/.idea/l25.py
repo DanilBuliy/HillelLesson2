5c5b691b53f8fe87d56fac9419c918197a3141e3
DanilBuliy <bulijdanil@gmail.com>
2023-08-04 11:29:13 +0300

    changed

--- Lesson/.idea/l25.py
+++ Lesson/.idea/l25.py
@@ -8,3 +8,17 @@ def generator(a):
 a=generator(6)
 for i in a:
     print(i)
+
+print("-"*50)
+
+def generator2(a):
+    b1 = 10
+    q = 3
+    for i in range(a):
+        yield b1
+        b1*=q
+
+a=generator2(6)
+for i in a:
+    print(i)
+
